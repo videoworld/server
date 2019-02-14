80c1933357332d7f1c3499e6158874c00f06b244
maria <msmariaulfa33@gmail.com>
2019-02-14 20:23:14 +0700

    upload

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 const router = require('express').Router()
 const videos = require('./videos')
+const images = require('../helpers/videos')
 
 router.use('/videos', videos)
+// router.post('/upload',
+//   images.multer.single('video'), 
+//   images.sendUploadToGCS,
+//   (req, res) => {
+//     console.log(res)
+//     res.send({
+//       status: 200,
+//       message: 'Your file is successfully uploaded',
+//       link: req.file.cloudStoragePublicUrl
+//     })
+//   })
+
 
 module.exports = router
 
